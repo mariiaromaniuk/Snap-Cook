b81f8d6d1b44dd86456a814cb4dd222cb675c8d9
Mariia Romaniuk <m.grabelnikova@gmail.com>
2020-07-17 11:25:46 -0400

    Main App file modified, App tests added

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import './shim' // hack for using rn-nodify
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Main from './src/'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Main />
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
